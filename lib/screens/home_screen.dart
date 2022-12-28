765c30c98a13a9374101e01686c7a3460dd49f4c
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2022-12-28 17:22:20 -0500

    slider de peliculas

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,8 +15,10 @@ class HomeScreen extends StatelessWidget {
                 onPressed: (() {}), icon: const Icon(Icons.search_outlined))
           ],
         ),
-        body: Column(
-          children: const [CardSwiper()],
+        body: SingleChildScrollView(
+          child: Column(
+            children: const [CardSwiper(), MovieSlider()],
+          ),
         ));
   }
 }
