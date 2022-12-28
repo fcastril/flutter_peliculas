8c783cab087d2c3a6e386b505fdd8864fbb3cecc
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2022-12-27 21:08:38 -0500

    Parte inicial de carrusel

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text('Home Screen'),
-    ));
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Películas en Cine'),
+          elevation: 0,
+          actions: [
+            IconButton(
+                onPressed: (() {}), icon: const Icon(Icons.search_outlined))
+          ],
+        ),
+        body: Column(
+          children: const [CardSwiper()],
+        ));
   }
 }
