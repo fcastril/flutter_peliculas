bda88accfff010cb10b02e574c2117bf8faabee6
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2022-12-28 20:05:48 -0500

    Foto de pelicula y titulos

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -11,8 +11,64 @@ class DetailsScreen extends StatelessWidget {
 
     return Scaffold(
         body: CustomScrollView(
-      slivers: [_CustomAppBar()],
+      slivers: [
+        _CustomAppBar(),
+        SliverList(delegate: SliverChildListDelegate([_PosterAndTitle()]))
+      ],
     ));
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme themeText = Theme.of(context).textTheme;
+    return Container(
+        margin: const EdgeInsets.only(top: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                height: 150,
+              ),
+            ),
+            const SizedBox(width: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('movie.title',
+                    style: themeText.headline5,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2),
+                Text('movie.originalTitle',
+                    style: themeText.subtitle1,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.star_outline,
+                      size: 15,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text('movie.voteAverage', style: themeText.caption)
+                  ],
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
 
@@ -26,16 +82,16 @@ class _CustomAppBar extends StatelessWidget {
       pinned: true,
       flexibleSpace: FlexibleSpaceBar(
         centerTitle: true,
-        titlePadding: EdgeInsets.all(0),
+        titlePadding: const EdgeInsets.all(0),
         title: Container(
             width: double.infinity,
             alignment: Alignment.bottomCenter,
             color: Colors.black12,
-            child: Text(
+            child: const Text(
               'movie.title',
               style: TextStyle(fontSize: 16),
             )),
-        background: FadeInImage(
+        background: const FadeInImage(
             placeholder: AssetImage('assets/loading.gif'),
             image: NetworkImage('https://via.placeholder.com/500x300'),
             fit: BoxFit.cover),
